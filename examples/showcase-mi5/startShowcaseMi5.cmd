274ddc6e2f3554ffb41c0b48edbf753439bd8609
Thomas Frei <mail@thomasfrei.de>
2016-03-09 22:29:47 +0100

    add batch start for showcase mi5, rename Order.js to Task.js

---
+++ examples/showcase-mi5/startShowcaseMi5.cmd
@@ -0,0 +1,16 @@
+@ECHO OFF
+ECHO This script will start a prodcution-plant example (Rabbit-MQ needed)
+START "DF" nodemon ./../../DFInstance.js
+pause
+ECHO Start the Agents for the production modules
+START "Cocktail" nodemon Cocktail.js
+START "Input" nodemon InputModule.js
+START "Output" nodemon OutputModule.js
+START "Cookie" nodemon Cookie.js
+START "ToppingBeckhoff" nodemon ToppingBeckhoff.js
+START "ToppingBosch" nodemon ToppingBosch.js
+pause
+ECHO Start the Agents for the transport modules
+START "XTS" nodemon HandlingXTS.js
+pause
+START "Task" nodemon Task.js
